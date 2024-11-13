e7f50b75593269dcfe8d87bc3246a6398e40ce06
SNGDNZ <thekaosity@gmail.com>
2024-11-13 17:59:56 +1100

    cleaned hud code

--- hud.gd
+++ hud.gd
@@ -1,7 +1,7 @@
 extends Control
 
-@onready var death_text = get_tree().get_first_node_in_group("you_died_text")
-@onready var death_text_timer = get_tree().get_first_node_in_group("death_text_timer")
+@onready var death_text = $YouDied
+@onready var death_text_timer = $DeathTextTimer
 
 func _on_death_text_timer_timeout() -> void:
 	death_text.visible_characters += 1
--- spawner.gd
+++ spawner.gd
@@ -4,7 +4,6 @@ extends Node2D
 @onready var player = get_tree().get_first_node_in_group("player")
 @export var enemy_max:int
 
-
 var time = 0
 
 func _on_timer_timeout():
