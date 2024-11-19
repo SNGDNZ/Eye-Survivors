3536614f3132437910f1e498492e4946bd991791
SNGDNZ <thekaosity@gmail.com>
2024-11-20 04:36:44 +1100

    incrimental orb improvement

--- Resources/orb.gd
+++ Resources/orb.gd
@@ -2,6 +2,7 @@ extends Area2D
 
 @onready var player = get_tree().get_first_node_in_group("player")
 @onready var orb_timeout_timer = $OrbTimeoutTimer
+@onready var orb_float_timer = $OrbFloatTimer
 @onready var sprite = $Sprite2D
 
 var damage = 4
@@ -15,22 +16,31 @@ var direction = Vector2.ZERO
 var angle:float
 var speed = 500
 var moving = true
+var orb_pos2_reached = false
 
 func _ready():
 	direction = position.direction_to(mousetarget)
 	angle = position.angle_to(mousetarget)
-
 	position.x = player.position.x - 576
 	position.y = player.position.y - 324
 	var moving = true
+	var orb_pos2_reached = false
 	
 
-func _physics_process(delta):
+func _process(delta):
 	if moving:
-		position += direction*speed*delta - pos1
+		if not orb_pos2_reached:
+			position += direction*speed*delta
+			if position.distance_to(pos2) < 100:
+				print(position.distance_to(pos2))
+				moving = false
+				orb_float_timer.start()
+				orb_pos2_reached
 
 
-
+func _on_orb_float_timer_timeout() -> void:
+	moving
+	
 
 func enemy_hit(charge = 1):
 	hp -= charge
--- Resources/weapon_handler.gd
+++ Resources/weapon_handler.gd
@@ -7,7 +7,6 @@ func _process(delta):
 	if Input.is_action_just_pressed("click"):
 		print(get_global_mouse_position())
 		orb_attack()
-		orb
 
 func orb_attack():
 	if player.isdead:
@@ -17,6 +16,7 @@ func orb_attack():
 	var orb_attack = orb.instantiate()
 	orb_attack.position = player.global_position
 	orb_attack.mousetarget = get_global_mouse_position() #VECTOR
-	orb_attack.pos1 = orb_attack.position + Vector2.from_angle(orb_attack.angle)*800
-	orb_attack.pos2 = orb_attack.position + Vector2.from_angle(orb_attack.angle)*300
+	orb_attack.pos1 = orb_attack.global_position + Vector2.from_angle(orb_attack.angle)*400
+	orb_attack.pos2 = orb_attack.global_position + Vector2.from_angle(orb_attack.angle)*300
 	add_child(orb_attack)
+	
