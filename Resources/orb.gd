635c9bc651848853390b996ebfbe975527f7bc62
SNGDNZ <thekaosity@gmail.com>
2024-11-20 21:37:18 +1100

    ORB WORKING

--- Resources/orb.gd
+++ Resources/orb.gd
@@ -1,36 +1,49 @@
 extends Area2D
 
 @onready var player = get_tree().get_first_node_in_group("player")
-@onready var orb_timeout_timer = $OrbTimeoutTimer
-@onready var orb_float_timer = $OrbFloatTimer
-@onready var sprite = $Sprite2D
 
+@onready var sprite = $Sprite2D
+@onready var orb_emit_snd = $OrbEmitSnd
+@onready var orb_float_snd = $OrbFloatSnd
+@onready var orb_impact_snd = $OrbImpactSnd
+@onready var orb_float_timer = $OrbFloatTimer
+@onready var orb_impact_timer = $OrbImpactTimer
 var damage = 4
 var hp = 1
+var knockback_amount = 600
 
 var mousetarget := Vector2.ZERO
-var pos1 := Vector2.ZERO
-var pos2 := Vector2.ZERO
+var targetpos1 := Vector2.ZERO
+var targetpos2 := Vector2.ZERO
 var angle := Vector2.ZERO
 var direction : float
-#var speed = 500
-#var moving = true
-#var orb_pos2_reached = false
 
-#func _ready():
+func _ready():
+	set_collision_mask_value(3, false)
+	set_collision_layer_value(3, false)
+	sprite.scale = Vector2(0,0)
+	orb_emit_snd.set_pitch_scale(randf_range(0.9,1.1))
+	orb_emit_snd.play()
+	orb_float_timer.start()
 
+func _on_orb_float_timer_timeout() -> void:
+	orb_float_snd.set_pitch_scale(randf_range(0.9,1.1))
+	orb_float_snd.play()
+	orb_impact_timer.start()
 
-#func _process(delta):
-	#position += direction*speed*delta
+func _on_orb_impact_timer_timeout() -> void:
+	orb_impact_snd.set_pitch_scale(randf_range(0.9,1.1))
+	orb_impact_snd.play(0.1)
+	set_collision_mask_value(3, true)
+	set_collision_layer_value(3, true)
 
-#func _on_orb_float_timer_timeout() -> void:
-	#moving
+func _on_orb_impact_snd_finished() -> void:
+	queue_free()
 
-func enemy_hit(charge = 1):
-	hp -= charge
-	if hp <= 0:
-		emit_signal("remove_from_array",self)
-		queue_free()
+#func enemy_hit(charge = 1):
+	#hp -= charge
+	#if hp <= 0:
+		#queue_free()
 
 func _on_orb_timeout_timer_timeout() -> void:
 	queue_free()
--- Resources/weapon_handler.gd
+++ Resources/weapon_handler.gd
@@ -1,40 +1,40 @@
 extends Node2D
 @onready var player = get_tree().get_first_node_in_group("player")
-
+@onready var enemy = get_tree().get_first_node_in_group("enemy")
 #ORB
-@onready var orb_float_timer = $OrbFloatTimer
-@onready var orb_impact_timer = $OrbImpactTimer
 var orb = preload("res://Resources/orb.tscn")
-
+var orb_pos2_reached = false
+var orb_attack = orb.instantiate()
 func _process(delta):
 	if Input.is_action_just_pressed("click"):
 		print("clickpos",get_global_mouse_position())
-		orb_attack()
+		orb_attack_func()
 
-func orb_attack():
+func orb_attack_func():
 	if player.isdead:
 		return
-	#if orb_attack_timer.is_stopped = false:
-		#return
-	var orb_attack = orb.instantiate()
-	#orb_attack.position = player.position
+	orb_attack = orb.instantiate()
 	orb_attack.global_position.x = player.position.x - 576
 	orb_attack.global_position.y = player.position.y - 324
+	
 	orb_attack.mousetarget = get_global_mouse_position() #VECTOR
 	orb_attack.direction = player.global_position.angle_to(orb_attack.mousetarget)
-	orb_attack.angle = player.global_position.direction_to(orb_attack.mousetarget*2*PI)
-	orb_attack.pos1 = orb_attack.global_position + Vector2.from_angle(orb_attack.direction)*200
-	orb_attack.pos2 = player.global_position + Vector2.from_angle(randf_range(0, 2*PI))*100
-	#orb_attack.pos2 = player.global_position + Vector2.from_angle(orb_attack.direction)*300
+	orb_attack.angle = enemy.global_position.direction_to(orb_attack.mousetarget)
+	
+	orb_attack.targetpos1 = player.global_position + Vector2.from_angle(randf_range(0, 2*PI))*100
+	orb_attack.targetpos2 = orb_attack.mousetarget
 	add_child(orb_attack)
 	#TWEEN
-	var tween_pos2 = orb_attack.create_tween()
-	tween_pos2.tween_property(orb_attack, "global_position", orb_attack.pos2,0.4).set_trans(Tween.TRANS_CIRC).set_ease(Tween.EASE_IN_OUT)
-	tween_pos2.tween_property(orb_attack, "scale", orb_attack.scale,0.2).set_trans(Tween.TRANS_CIRC).set_ease(Tween.EASE_IN_OUT)
-	tween_pos2.play
-	orb_float_timer.start()
+	var orb_tween = orb_attack.create_tween().set_parallel(true)
+	orb_tween.tween_property(orb_attack.sprite, "scale", Vector2(1,1),0.2).set_trans(Tween.TRANS_CIRC).set_ease(Tween.EASE_IN)
+	orb_tween.tween_property(orb_attack, "global_position", orb_attack.targetpos1,0.4).set_trans(Tween.TRANS_CIRC).set_ease(Tween.EASE_IN_OUT)
+	orb_tween.chain().tween_property(orb_attack.sprite, "scale", Vector2(1,1),0.1).set_trans(Tween.TRANS_ELASTIC).set_ease(Tween.EASE_IN_OUT)
+	orb_tween.chain().tween_property(orb_attack, "global_position", orb_attack.mousetarget,0.3).set_trans(Tween.TRANS_CIRC).set_ease(Tween.EASE_IN_OUT)
+	orb_tween.play
 	#DEBUG
-	print("mousetarget",orb_attack.mousetarget)
-	print("playerglobalpos",player.global_position)
-	print("playerpos",player.position)
-	print("pos2",orb_attack.pos2)
+	#print("mousetarget",orb_attack.mousetarget)
+	#print("playerglobalpos",player.global_position)
+	#print("playerpos",player.position)
+	#print("pos2",orb_attack.targetpos2)
+	
+	
